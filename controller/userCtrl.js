b51cf3738d9ae3285332b7996bc9bda9d4c8b92a
mdsujon428 <mdsujon000056@outlook.com>
2023-01-20 17:14:41 +0600

    express error handler and password encryption logic added

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -1,39 +1,28 @@
 const User = require('../models/userModel');
-
-const createUser = async (req, res) => {
-    // const {email} = req.body;
-    const { email, firstName, mobile, password } = req.body;
-
-    if (firstName && email && mobile && password) {
-        try {
-            const findUser = await User.findOne({ email });
-        if (!findUser) {
-            //create user
-            const newUser = await User.create(req.body);
-            res.status(200).json({
-                ...newUser,
-                message: "User created",
-                status:true
-            });
-        } else {
-            res.status(500).json({
-                message: "This user is exist",
-                success:false
-            })
-        }
-        } catch (error) {
-            res.json({
-                ...error,
-                message: "Failed to create account",
-                success:false
-           })
-        }
+const asyncHandler = require('express-async-handler');
+const createUser = asyncHandler(async (req, res) => {
+    const { email } = req.body;
+    const findUser = await User.findOne({ email });
+    console.log(Boolean(findUser))
+    if (!findUser) {
+        const newUser = await User.create(req.body);
+        res.json(newUser);
     } else {
-        res.status(500).json({
-           
-        })
+        throw new Error("User Already Exist");
     }
-  
-}
+})
 
-module.exports ={createUser}
+const loginUserCtrl = asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+    // if user exists or not 
+    const findUser = await User.findOne({ email });
+    if (findUser && findUser.isPasswordMatched(password)) {
+       res.json(findUser)
+    } else {
+        throw new Error("Email or password is Invalid")
+    }
+    
+})
+
+
+module.exports ={createUser,loginUserCtrl}
--- index.js
+++ index.js
@@ -6,17 +6,18 @@ const PORT = process.env.PORT || 8081;
 const { default: mongoose, model } = require('mongoose');
 const authRoute = require('./routes/authRoute');
 const dbConnect = require('./config/dbConnect');
-// const { default:mongoose } = require('mongoose'); 
-
-// console.log("FROM HERE.......................... ",mongoose.set())
-
+const { notFound, errorHandler } = require('./middlewares/errorHandler');
 
 dbConnect();
+app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:false}));
 // app.use("/", (req, res) => {
 //     res.send(`E commerce server is running at port ${PORT}`)
 // })
 app.use('/api/user',authRoute)
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
     console.log(`E commerce server is running at port ${PORT}`)
