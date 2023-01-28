b4d4ffb69b7d8dfc0b8ced384b287b5d711b6978
mdsujon428 <mdsujon000056@outlook.com>
2023-01-28 12:47:03 +0600

    update product operation made

--- controller/productCtrl.js
+++ controller/productCtrl.js
@@ -6,13 +6,27 @@ const slugify = require('slugify')
 const createProduct = asyncHandler(async (req, res) => {
     try {
         if (req.body.title) {
-            req.body.slug =slugify( req.body.title);
+            req.body.slug = slugify( req.body.title);
         }
         const newProduct = await Product.create(req.body);
         res.json(newProduct)
     } catch (error) {
        
         throw new Error(error)
+    }
+})
+
+// update product
+const updateProduct = asyncHandler(async (req, res) => {
+    const id = req.params;
+    try {
+        if (req.body.title) {
+            req.body.slug = slugify(req.body.title);
+        }
+        const updateProduct = await Product.findOneAndUpdate({ id }, req.body, { new: true });
+        res.json(updateProduct)
+    } catch (error) {
+        throw new Error(error);
     }
 })
 
@@ -37,4 +51,4 @@ const getAllProduct = asyncHandler(async(req, res) =>{
     }
 })
 
-module.exports ={createProduct,getAProduct,getAllProduct }
+module.exports ={createProduct,getAProduct,getAllProduct,updateProduct }
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { createProduct,getAProduct ,getAllProduct} = require('../controller/productCtrl');
+const { createProduct,getAProduct ,getAllProduct,updateProduct} = require('../controller/productCtrl');
 // const { route } = require('./authRoute');
 const router = express.Router();
 
 router.post("/", createProduct);
 router.get("/",getAllProduct)
 router.get("/:id", getAProduct);
+router.put('/:id', updateProduct);
 module.exports = router
