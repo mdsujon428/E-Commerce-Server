8dc07d327b881d4a9d1ce0fcab795b55fd4e4db5
mdsujon428 <mdsujon000056@outlook.com>
2023-02-03 17:40:17 +0600

    blog Model or blog schema is ready

---
+++ models/blogModel.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose'); // Erase if already required
+
+// Declare the Schema of the Mongo model
+var blogSchema = new mongoose.Schema({
+    title:{
+        type:String,
+        required:true,
+    },
+    description:{
+        type:String,
+        required:true,
+    },
+    category:{
+        type:String,
+        required:true,
+    },
+    numViews:{
+        type:String,
+        default:0, 
+    },
+    isLiked:{
+        type:Boolean,
+        default:false, 
+    },
+    isDisliked:{
+        type:Boolean,
+        default:false, 
+    },
+    likes: [
+        {
+            type: mongoose.Schema.ObjectId,
+            ref:"User"
+        }
+    ],
+    dislikes: [
+        {
+            type: mongoose.Schema.ObjectId,
+            ref:"User"
+        }
+    ],
+    images: {
+        type: String,
+        default:"https://plus.unsplash.com/premium_photo-1663852297654-56d979cf72d2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+    },
+    author: {
+        type: String,
+        default:"Admin"
+    }
+}, {
+    toJSON: {
+        virtuals:true
+    },
+    toObject: {
+        virtuals:true
+    },
+    timestamps:true
+});
+
+//Export the model
+module.exports = mongoose.model('Blog', blogSchema);
