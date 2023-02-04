78461f97c7b3f2e6ed44999a0a0d48698d06913a
mdsujon428 <mdsujon000056@outlook.com>
2023-02-04 18:20:51 +0600

    category model made

--- controller/blogCtrl.js
+++ controller/blogCtrl.js
@@ -35,7 +35,9 @@ const getBlog = asyncHandler(async (req, res) => {
     const { id } = req.params;
     validateMongoDbId(id)
     try {
-        const getBlog = await Blog.findById(id);
+        const getBlog = await Blog.findById(id)
+            .populate('likes')
+            .populate('dislikes');
         const updateViews = await Blog.findByIdAndUpdate(
             id,
             {
@@ -43,7 +45,7 @@ const getBlog = asyncHandler(async (req, res) => {
             },
             {new : true}
         )
-        res.json(updateViews)
+        res.json(getBlog)
     } catch (error) {
         throw new Error(error);
     }
