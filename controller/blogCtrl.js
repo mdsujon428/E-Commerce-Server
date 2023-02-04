03bbc4819448253487250cc0d05f0e8f14027cd9
mdsujon428 <mdsujon000056@outlook.com>
2023-02-04 10:23:24 +0600

    blog update operation made

--- controller/blogCtrl.js
+++ controller/blogCtrl.js
@@ -3,6 +3,7 @@ const User = require('../models/userModel');
 const asyncHandler = require('express-async-handler')
 const { validateMongoDbId } = require('../utils/validateMongodbId');
 
+// create blog
 const createBlog = asyncHandler(async (req, res) => {
     try {
         const newBlog = await Blog.create(req?.body);
@@ -14,4 +15,22 @@ const createBlog = asyncHandler(async (req, res) => {
     }
 })
 
-module.exports ={ createBlog}
+// update blog
+const updateBlog = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+    try {
+        const updateBlog = await Blog.findByIdAndUpdate(id, req.body, {
+            new:true
+        });
+        res.json({
+            updateBlog
+        })
+    } catch (error) {
+        throw new Error(error);
+    }
+})
+
+module.exports = {
+    createBlog,
+    updateBlog
+}
--- routes/blogRoute.js
+++ routes/blogRoute.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const { isAdmin, authMiddleWare } = require('../middlewares/authMiddleware');
-const {createBlog} =  require('../controller/blogCtrl')
+const {
+    createBlog,
+    updateBlog
+} = require('../controller/blogCtrl')
 const router = express.Router();
 
 router.post("/",authMiddleWare,isAdmin,createBlog)
+router.put("/:id",authMiddleWare,isAdmin,updateBlog)
 
 module.exports = router;
